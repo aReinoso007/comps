c7aacef40a131fa7e5063fd808a26e9718705160
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-08-07 19:07:15 -0500

    state for accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function Accordion({
     items
 }){
-    const renderedItems = items.map((item)=>{
+
+    const [ expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index)=>{
+        const isExpanded = index === expandedIndex
         return (
             <div key={item.id}>
                 <div>
