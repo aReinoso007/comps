edf6ac134f9ceb734c848b1a2dce0b43c9686ff0
Alex Reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-10-22 21:08:51 -0500

    icons
    icons on expanded state

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0);
@@ -10,9 +11,16 @@ function Accordion({ items }) {
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const icon = <span>
+      {isExpanded ? <GoChevronDown/> : <GoChevronLeft/> }
+    </span>
+
     return (
       <div key={item.id}>
-        <div onClick={() => handleClick(index)}>{item.label}</div>
+        <div onClick={() => handleClick(index)}>
+          {icon}
+          {item.label}
+          </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
