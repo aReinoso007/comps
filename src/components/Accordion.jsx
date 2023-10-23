b5a7ba4c363e04102e33ac2969d3398bd533d041
Alex Reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-10-22 21:01:25 -0500

    Update Accordion.jsx
    format

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,38 +1,24 @@
 import { useState } from "react";
 
-function Accordion({
-    items
-}){
+function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(0);
 
-    const [ expandedIndex, setExpandedIndex] = useState(0);
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
 
-    const handleClick = (nextIndex) => { 
-        setExpandedIndex(nextIndex)
-    }
-
-    const renderedItems = items.map((item, index)=>{
-        const isExpanded = index === expandedIndex
-
-        
-
-        return (
-            <div key={item.id}>
-                <div onClick={
-                    ()=>handleClick(index)
-                }>
-                    {item.label}
-                </div>
-                {isExpanded && <div>{item.content}</div>}
-            </div>
-        )
-    });
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
 
     return (
-    <div>
-        {renderedItems}
-    </div>
-    )
-    
+      <div key={item.id}>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
