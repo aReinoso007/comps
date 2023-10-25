837134a29cfb5f791043e11645e8273588652dbe
Alex Reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-10-25 08:42:25 -0500

    organizing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "RED", value: "red" },
-    { label: "GREEN", value: "green" },
-    { label: "BLUE", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
-  );
+  return <div></div>
 }
 
 export default App;
---
+++ src/pages/DropdownPage.jsx
@@ -0,0 +1,23 @@
+import Dropdown from "../components/Dropdown";
+import { useState } from "react";
+function DropwdownPage() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: "RED", value: "red" },
+    { label: "GREEN", value: "green" },
+    { label: "BLUE", value: "blue" },
+  ];
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
+}
+
+export default DropwdownPage;
