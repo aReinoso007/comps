d59253a93c2cffe7f989c551e0c8bc81135fbc82
Alex Reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-10-25 21:50:02 -0500

    route component and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Link from "./components/Link";
+import Route from "./components/Route";
+import AccordionPage from './pages/AccordionPage'
+import DropdownPage from "./pages/DropdownPage";
+
 function App() {
-  return <div></div>
+  return <div>
+    <Link to="/accordion">Go to accordion</Link>
+    <Link to="/dropdown">Go to dropdown</Link>
+    <div>
+      <Route path="/accordion">
+        <AccordionPage/>
+      </Route>
+
+      <Route path="/dropdown">
+        <DropdownPage />
+      </Route>
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/components/Link.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import NavigationContext from "../context/navigation";
+
+function Link({to, children}){
+    /*
+    accessing navigation context
+    */
+    const {navigate} = useContext(NavigationContext);
+
+    const handleClick = (event) =>{
+        event.preventDefault();
+        navigate(to)
+    }
+
+    return <a onClick={handleClick}>{children}</a>
+}
+
+export default Link;
