ad871c80dad4b39aa3e7c3cb60c13775bdb546b9
Alex Reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-10-25 21:23:41 -0500

    navigation context

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { NavigationProvider } from './context/navigation';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-    <App />
+   <NavigationProvider>
+     <App />
+   </NavigationProvider>
 )
