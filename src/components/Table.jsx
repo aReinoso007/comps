e5d9867b0b0c2ad3952cf8c378df82e8ee9b5141
Alex Reinoso <ralexjessiel@gmail.com>
2023-10-31 20:19:29 -0500

    render table with data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,29 @@
-function Table({data}){
+function Table({ data }) {
+  const tableHeader = data.map((item, index) => (
+    <th key={index}>{item.name}</th>
+  ));
 
-    return(
-        <div>
-           Table
-        </div>
-    )
+  const renderedRows = data.map((item, index) => (
+    <tr key={index} className="border-b">
+      <td className="p-3">{item.name}</td>
+      <td className="p-3">
+        <div className={`p-3 m-3 ${item.color}`}></div>
+      </td>
+      <td className="p-3">{item.score}</td>
+    </tr>
+  ));
+
+  return (
+    <table className="table-auto border-spacing-2">
+        <thead>
+            <tr className="border-b-2">
+                {tableHeader}
+            </tr>
+        </thead>
+        <tbody>
+            {renderedRows}
+        </tbody>
+    </table>
+  );
 }
 export default Table;
