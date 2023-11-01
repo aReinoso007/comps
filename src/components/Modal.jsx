d7de80edf3dc3c8ffd51effc0df313a7f3b99cf6
Alex Reinoso <ralexjessiel@gmail.com>
2023-10-31 19:52:56 -0500

    modal fix

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -18,9 +18,9 @@ function Modal({ onClose, children, actionBar }) {
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
+      <div className="fixed inset-40 p-10 bg-white">
         <div className="flex flex-col justify-between h-full">
           {children}
           <div className="flex justify-end">
