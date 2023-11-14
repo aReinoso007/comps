ef2fdac1ea4f2df9a8b532eea5f11f903bf4f2b9
Alex Reinoso <ralexjessiel@gmail.com>
2023-11-13 22:09:51 -0500

    show arrows on sort value

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,62 +1,94 @@
 import { useState } from "react";
+import { GoChevronUp, GoChevronDown } from "react-icons/go";
 import Table from "./Table";
 
-function SortableTable(props){
+function SortableTable(props) {
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
 
-    const [sortOrder, setSortOrder] = useState(null);
-    const [sortBy, setSortBy] = useState(null);
-    const {config, data } = props;
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder === 'asc') {
+      setSortOrder('desc');
+      setSortBy(label);
+    } else if (sortOrder === 'desc') {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
 
 
-    const handleClick = (label) => {
-        if(sortOrder === null){
-            setSortOrder('asc');
-            setSortBy(label)
-        }else if(sortOrder === 'asc'){
-            setSortOrder('desc');
-            setSortBy(label)
-        } else if (sortOrder === 'desc'){
-            setSortOrder(null);
-        }   
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
     }
 
-    const updatedConfig = config.map((column) => {  
-        if(!column.sortValue){
-            return column;
-        }
-        return {
-            ...column,
-            header: () => <th onClick={()=> handleClick(column.label)}>{column.label} Is Sortable</th>
-        }
-    } );
-    /**
-     * Only sort data if sortOrder and sortBy are not null
-     * if they are not null make a copy of the original data
-     * and sort the copy
-     * get the sortValue from the config
-     */
+    return {
+      ...column,
+      header: () => (
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => handleClick(column.label)}
+        >
+          <div className="flex items-center">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
+        </th>
+      ),
+    };
+  });
 
-    let sortedData = data;
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
 
-    if (sortOrder && sortBy){
-        const {sortValue } = config.find(column => column.label === sortBy);
-        sortedData = [...data].sort((a,b) => {
-            const valueA = sortValue(a);
-            const valueB = sortValue(b);
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
 
-            const reverseOrder = sortOrder === 'desc' ? -1 : 1;
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
 
-            if (typeof valueA === 'string'){
-                return reverseOrder * valueA.localeCompare(valueB);
-            }else{
-                return (valueA - valueB)* reverseOrder;
-            }
-        });
-    }
-
-    return <div>
-        <Table {...props} data={sortedData} config={updatedConfig}/>
+  return (
+    <div>
+      <Table {...props} data={sortedData} config={updatedConfig} />
     </div>
-
+  );
 }
+
+
+function getIcons(label, sortBy, sortOrder) {
+    if(label !== sortBy){
+        return <div>
+            <GoChevronUp/>
+            <GoChevronDown/>
+        </div>
+    }
+
+    if(sortOrder === null){
+        return <div>
+            <GoChevronUp/>
+            <GoChevronDown/>
+        </div>
+    }else if(sortOrder === 'asc'){
+        return <div>
+        <GoChevronUp/>
+    </div>
+    }else if(sortOrder === 'desc'){
+        return <div>
+        <GoChevronDown/>
+    </div>
+    }
+}
+
 export default SortableTable;
