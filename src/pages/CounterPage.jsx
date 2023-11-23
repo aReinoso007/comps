f06792ee71aa957e2b4b916ed9ea34ed9c959d8f
Alex Reinoso <ralexjessiel@gmail.com>
2023-11-22 21:59:07 -0500

    working on custom hook

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import Button from "../components/Button";
 
-function CounterPage({initialCount}){
-
+function useSomething({initialCount}){
     const [count, setCount] = useState(initialCount);
 
     useEffect(() => {
-        console.log(count);   
-
     }   , [count]);
 
     const handleClick = () => { 
         setCount(count + 1);
     }
 
+    return {
+        count,
+        handleClick,
+    }
+}
+
+function CounterPage({initialCount}){
+
+    const {count, handleClick} = useSomething({initialCount});  
 
     return (
         <div>
